e6d5c21de57f15b7f9c5dd1daa32fd6ef7c19fa4
Mr.Mao <951416545@qq.com>
2022-03-31 04:06:52 +0800

    chore: clear code

--- src/layout/Client.vue
+++ src/layout/Client.vue
@@ -9,22 +9,13 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent, ref, onMounted } from 'vue-demi'
+  import { defineComponent } from 'vue-demi'
   export default defineComponent({ name: 'ClientLayout' })
 </script>
 <script lang="ts" setup>
   import ClientHeader from './components/ClientHeader.vue'
   import ClientSidebar from './components/ClientSidebar.vue'
   import ClientBody from './components/ClientBody.vue'
-  import { Game } from '@/typings'
-  import { getGames } from '@/api/index.mock'
-
-  const games = ref<Game[]>([])
-
-  onMounted(async () => {
-    const { data } = await getGames()
-    games.value = data
-  })
 </script>
 
 <style lang="scss" scoped>
